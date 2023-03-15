e01635cc5fc99192a398392c67de5bd517ae0085
Karolina <162272@student.uwm.edu.pl>
2023-03-15 15:17:32 +0100

    [BE][5]integrate with SQLAlchemy

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Account
+from flask_migrate import Migrate
+from models import Account
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:5432/account'
-db.init_app(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'http://postgres:projekt1234@localhost:80/account'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, compare_type=True)
+
+class account(db.Model):
+    account_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    email = db.Column(db.String(20))
+
+def __init__(self, username, password, email):
+    self.username = username
+    self.password = password
+    self.email = email
 
 @app.route('/')
 def main():
@@ -16,11 +31,14 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        account = Account(username=username, password=password, email=email)
-        db.session.add(account)
+        user_account = account(username=username, password=password, email=email)
+        db.session.add(user_account)
         db.session.commit()
-        return 'Udana rejestracja'
-    return 'Aktywne'
+        flash('Udana rejestracja')
+        return redirect(url_for('main'))
+    return '!'
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
+    
--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
+from werkzeug.security import generate_password_hash
 
 class Account(db.Model):
-    __tablename__ = "account"
-    account_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(20), unique=True)
+    __tablename__='account'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+
+    # def set_password(self, password):
+    #     self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return '<Account {}>'.format(self.email)
