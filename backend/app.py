67d1a02e69c79bfd0ac4fdc0d20dbc379404ef39
Karolina <162272@student.uwm.edu.pl>
2023-03-15 22:08:40 +0100

    [BE][5]sqlalchemy fix?

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,21 @@
 from flask import Flask, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import Account
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'http://postgres:projekt1234@localhost:80/account'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:projekt1234@localhost:80/account'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, compare_type=True)
 
-class account(db.Model):
-    account_id = db.Column(db.Integer, primary_key=True)
+class Account(db.Model):
+    __tablename__='account'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20))
     username = db.Column(db.String(20))
     password = db.Column(db.String(20))
-    email = db.Column(db.String(20))
-
-def __init__(self, username, password, email):
-    self.username = username
-    self.password = password
-    self.email = email
 
 @app.route('/')
 def main():
@@ -31,14 +27,12 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        user_account = account(username=username, password=password, email=email)
+        user_account = Account(username=username, password=password, email=email)
         db.session.add(user_account)
         db.session.commit()
         flash('Udana rejestracja')
         return redirect(url_for('main'))
-    return '!'
+    return 'no POST request :)'
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
-    
