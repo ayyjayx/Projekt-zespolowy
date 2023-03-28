e984c5a662fe72efdfbe2e28a9f59525d38059a8
Karolina <karolina.ryzinska@student.uwm.edu.pl>
2023-03-29 00:01:51 +0200

    [BE][10] flake8 black, pre-commit

--- backend/models.py
+++ backend/models.py
@@ -11,36 +11,50 @@ class Account(db.Model):
     id = db.Column(db.INTEGER(), primary_key=True, autoincrement=True)
     email = db.Column(db.VARCHAR(50), nullable=False, unique=True)
     username = db.Column(db.VARCHAR(50), nullable=False, unique=True)
-    password = db.Column(db.VARCHAR(50), nullable=False)
+    password = db.Column(db.VARCHAR(256), nullable=False)
     created_on = db.Column(postgresql.TIMESTAMP(), autoincrement=False)
-    last_login = db.Column(postgresql.TIMESTAMP(), autoincrement=False, nullable=True)
-    
+    last_login = db.Column(postgresql.TIMESTAMP(), autoincrement=False,
+                           nullable=True)
+
+
 class Player(db.Model):
     __tablename__ = "player"
-    id = db.Column(db.INTEGER(), autoincrement=True, nullable=False, primary_key=True)
-    player_id = db.Column(db.INTEGER(), db.ForeignKey('account.id'), autoincrement=False, nullable=False)
-    game_id = db.Column(db.INTEGER(), db.ForeignKey('game.id'), autoincrement=False, nullable=False)
+    id = db.Column(db.INTEGER(), autoincrement=True, nullable=False,
+                   primary_key=True)
+    player_id = db.Column(db.INTEGER(), db.ForeignKey('account.id'),
+                          autoincrement=False, nullable=False)
+    game_id = db.Column(db.INTEGER(), db.ForeignKey('game.id'),
+                        autoincrement=False, nullable=False)
 
     player_game_id_fkey = db.relationship('Game', foreign_keys='Player.game_id')
     player_player_id_fkey = db.relationship('Account', foreign_keys='Player.player_id')
+
 
 class Result(db.Model):
     __tablename__ = "result"
     id = db.Column(db.INTEGER(), autoincrement=True, nullable=False, primary_key=True)
     description = db.Column(db.VARCHAR(length=128), autoincrement=False, nullable=True)
-    postgresql_ignore_search_path=False
+    postgresql_ignore_search_path = False
+
 
 class Game(db.Model):
     __tablename__ = "game"
-    id = db.Column(db.BIGINT(), primary_key=True, autoincrement=True, nullable=False)
-    start_time = db.Column(db.TIMESTAMP(), autoincrement=False, nullable=False)
-    end_time = db.Column(db.TIMESTAMP(), autoincrement=False, nullable=True)
-    player_one_id = db.Column(db.INTEGER(), db.ForeignKey('account.id'), autoincrement=False, nullable=False)
-    player_two_id = db.Column(db.INTEGER(), db.ForeignKey('account.id'), autoincrement=False, nullable=False)
-    result_id = db.Column(db.INTEGER(), db.ForeignKey('result.id'), autoincrement=False, nullable=True)
+    id = db.Column(db.BIGINT(), primary_key=True, autoincrement=True,
+                   nullable=False)
+    start_time = db.Column(db.TIMESTAMP(), autoincrement=False,
+                           nullable=False)
+    end_time = db.Column(db.TIMESTAMP(), autoincrement=False,
+                         nullable=True)
+    player_one_id = db.Column(db.INTEGER(), db.ForeignKey('account.id'),
+                              autoincrement=False, nullable=False)
+    player_two_id = db.Column(db.INTEGER(), db.ForeignKey('account.id'),
+                              autoincrement=False, nullable=False)
+    result_id = db.Column(db.INTEGER(), db.ForeignKey('result.id'),
+                          autoincrement=False, nullable=True)
 
-    game_result_id_fkey = db.relationship('Result', foreign_keys='Game.result_id')
-    game_player_one_id_fkey = db.relationship('Account', foreign_keys='Game.player_one_id')
-    game_player_two_id_fkey = db.relationship('Account', foreign_keys='Game.player_two_id')
-
-
+    game_result_id_fkey = db.relationship('Result',
+                                          foreign_keys='Game.result_id')
+    game_player_one_id_fkey = db.relationship('Account',
+                                              foreign_keys='Game.player_one_id')
+    game_player_two_id_fkey = db.relationship('Account',
+                                              foreign_keys='Game.player_two_id')
--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, session, render_template, url_for, redirect, flash
+from flask import request, session, render_template, url_for, redirect, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import LoginForm, RegistrationForm
 from models import db, Account
@@ -8,13 +8,13 @@ from datetime import datetime
 
 def init_routes(app):
 
-    @app.route('/', methods=['GET'])
-    def main():
+    @app.route('/start', methods=['GET'])
+    def start():
         if session.get('logged_in'):
             return redirect(url_for('home'))
         else:
             return redirect(url_for('index'))
-        
+
     @app.route('/home', methods=['GET'])
     def home():
         return render_template('home.html')
@@ -23,44 +23,40 @@ def init_routes(app):
     def index():
         return render_template('index.html')
 
-    @app.route("/register", methods=["POST"])
+    @app.route("/register", methods=['GET', 'POST'])
     def register():
-        if request.method == 'POST':
-            form = RegistrationForm()
-            if form.validate_on_submit():
-                username = form.username.data
-                email = form.email.data
-                password = form.password.data
-                hashed = generate_password_hash(password, method='sha256')
-                    try:
-                        new_user = Account(
-                            username=username,
-                            email=email,
-                            password=hashed,
-                            created_on=datetime.utcnow())
-                        db.session.add(new_user)
-                        db.session.commit()
-                    except exc.IntegrityError:
-                        flash('Username/Email already in use!', 'error')
-                        return redirect(url_for('register'))
-                    session['logged_in'] = True
-                    flash('Successful Registration :)', 'success')
-                    return redirect(url_for('/'))
-            else:
-                flash('Fill missing fields!', 'error')
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+            hashed = generate_password_hash(password)
+            try:
+                new_user = Account(
+                    username=username,
+                    email=email,
+                    password=hashed,
+                    created_on=datetime.utcnow())
+                db.session.add(new_user)
+                db.session.commit()
+            except exc.IntegrityError:
+                flash('Username/Email already in use!', 'error')
                 return redirect(url_for('register'))
-        else: return render_template('register.html', form=form)
+            session['logged_in'] = True
+            flash('Successful Registration :)', 'success')
+            return redirect(url_for('start'))
+        return render_template('register.html', form=form)
 
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         form = LoginForm()
         if form.validate_on_submit():
-            account = Account.query.get(form.username.data)
+            account = Account.query.filter_by(username=form.username.data).first()
             if account:
                 if check_password_hash(account.password, form.password.data):
                     session['logged_in'] = True
                     flash('Successful Login :)', 'success')
-                    return redirect(url_for('/')) 
+                    return redirect(url_for('start'))
                 else:
                     flash('Wrong Password!', 'error')
                     return redirect(url_for('login'))
@@ -68,9 +64,31 @@ def init_routes(app):
                 flash('Account does not exist!', 'error')
                 return redirect(url_for('login'))
         return render_template('login.html', form=form)
-        
+
     @app.route('/logout', methods=['GET', 'POST'])
     def logout():
         session['logged_in'] = False
-        flash('Successfuly Logged out', 'success')
-        return redirect(url_for('/'))
+        flash('Successfully Logged out', 'success')
+        return redirect(url_for('start'))
+
+    @app.route('/admin/create', methods=['GET', 'POST'])
+    def create():
+        if request.method == 'GET':
+            return render_template('create.html')
+        if request.method == 'POST':
+            form = RegistrationForm()
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+            hashed = generate_password_hash(password)
+            try:
+                new_user = Account(
+                    username=username,
+                    email=email,
+                    password=hashed,
+                    created_on=datetime.utcnow())
+                db.session.add(new_user)
+                db.session.commit()
+            except exc.IntegrityError:
+                flash('Username/Email already in use!', 'error')
+                return redirect(url_for('/admin/create'))
