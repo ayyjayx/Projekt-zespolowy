7de89c368a839c848f849af5f5425c0381aa4bbe
Karolina <karolina.ryzinska@student.uwm.edu.pl>
2023-04-05 22:28:40 +0200

    [BE][8] CRUD + jwt_extended

--- backend/routes.py
+++ backend/routes.py
@@ -3,40 +3,7 @@ import jwt
 from config import Config
 from flask import jsonify, make_response, request
 from models import Account, db, JWTTokenBlocklist
-from functools import wraps
-
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if "Authorization" in request.headers:
-            token = request.headers["Authorization"]
-        if not token:
-            return jsonify({"message": "Token is missing !!"}), 400
-
-        try:
-            data = jwt.decode(token, Config.SECRET_KEY)
-            current_user = Account.query.filter_by(id=data["id"]).first()
-
-            if not current_user:
-                return jsonify({"message": "Account does not exist."}), 400
-
-            token_expired = db.session.query(JWTTokenBlocklist.id)\
-                .filter_by(jwt_token=token).scalar()
-
-            if token_expired is not None:
-                return jsonify({"message": "Token expired."}), 400
-
-            if not current_user.check_jwt():
-                return jsonify({"message": "Token expired."}), 400
-
-        except:
-            return jsonify({"message": "Token is invalid."}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 
 
 def init_routes(app):
@@ -94,21 +61,15 @@ def init_routes(app):
                 return make_response("Account does not exist.", 401)
 
             if account.check_password(password):
-                token = jwt.encode({
-                    "id": account.id,
-                    "username": account.username,
-                    "admin": account.admin,
-                    "exp": datetime.utcnow() + timedelta(minutes=30)},
-                    Config.SECRET_KEY
-                )
-                return jsonify({"token": token}), 300
+                token = create_access_token(identity=account.id)
+                return jsonify({"token": token})
 
             return make_response("Incorrect Password.", 403)
         else:
             return make_response("Use POST", 200)
 
     @app.route("/logout", methods=["GET", "POST"])
-    @token_required
+    @jwt_required()
     def logout():
         token = request.headers["Authorization"]
 
@@ -116,11 +77,28 @@ def init_routes(app):
         jwt_block.save()
 
         return make_response("Token expired.", 200)
+        
+    @app.route("/account", methods=["GET"])
+    @jwt_required()
+    def show_account():
+        current_user = get_jwt_identity()
+        account = Account.query.get(current_user)
 
-    @app.route("/admin/accounts/edit/<int:account_id>", methods=["GET", "POST"])
-    @token_required
-    def edit_account(account_id):
-        account = Account.query.filter_by(id=account_id).first()
+        if not account:
+            return make_response("Account does not exist.", 404)
+        
+        return jsonify({
+            "id": account.id,
+            "email": account.email,
+            "username": account.username,
+            "created_on": account.created_on}), 200
+
+    @app.route("/account/update", methods=["GET", "POST"])
+    @jwt_required()
+    def edit_account():
+        data = request.get_json()
+        id = data.get("id")
+        account = Account.query.filter_by(id=id).first()
 
         if not account:
             return make_response("Account does not exist", 404)
@@ -129,9 +107,19 @@ def init_routes(app):
             data = request.get_json()
             new_username = data.get("username")
             new_email = data.get("email")
+            new_password = data.get("password")
+            new_passwordRepeat = data.get("passwordRepeat")
 
-            account.update_email(new_email)
-            account.update_username(new_username)
+            if new_username is not None:
+                account.update_username(new_username)
+
+            if new_email is not None:
+                account.update_email(new_email)
+
+            if new_password is not None and new_passwordRepeat is not None:
+                if new_password == new_passwordRepeat:
+                    account.update_password(new_password)
+
             account.save()
 
             return make_response("Account successfully updated.", 200)
@@ -140,3 +128,17 @@ def init_routes(app):
             "id": account.id,
             "email": account.email,
             "username": account.username}), 200
+
+    @app.route("/account/delete", methods=["DELETE"])
+    @jwt_required()
+    def delete_account():
+        data = request.get_json()
+        id = data.get("id")
+        account = Account.query.filter_by(id=id).first()
+
+        if not account:
+            return make_response("Account does not exist", 404)
+        
+        account.delete()
+
+        return make_response("Account successfully deleted.", 200)
