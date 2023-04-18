47817479e09600c15fbecaffaed85ca1e8fc4a21
Karolina <karolina.ryzinska@student.uwm.edu.pl>
2023-04-18 21:07:59 +0200

    [BE][4] reset password

--- backend/config.py
+++ backend/config.py
@@ -10,6 +10,12 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=1)
     JWT_COOKIE_SECURE = False
+    MAIL_SERVER = 'sandbox.smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USERNAME = 'b11e69d7674e43'
+    MAIL_PASSWORD = '59478b0bd8b607'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
