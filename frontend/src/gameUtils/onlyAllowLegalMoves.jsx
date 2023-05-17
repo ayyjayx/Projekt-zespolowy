ec5662a5ba1e6c824774a44489e99ad1ad9dcd8a
Karolina <karolina.ryzinska@student.uwm.edu.pl>
2023-05-17 21:40:40 +0200

    [BE] merge conflicts

--- frontend/src/gameUtils/onlyAllowLegalMoves.jsx
+++ frontend/src/gameUtils/onlyAllowLegalMoves.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Chess } from 'chess.js';
 import 'chessboard-element';
 import axios from "axios";
