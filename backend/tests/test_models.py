094f94db2e2f51c265a50b24216eb9a3422a3202
Konrad <goostaw.projekt@gmail.com>
2023-05-28 19:20:16 +0200

    tests update (pytest parametrize)

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from models import Account
+from models import Account, ResetToken, Game
 
 
 def test_account(new_account, app):
@@ -8,6 +8,7 @@ def test_account(new_account, app):
 
         # add new account to database, check is it exists
         account = Account.query.filter_by(email="pytest@test.com").first()
+
         assert account is not None
 
         # check if all data is as input
@@ -17,3 +18,63 @@ def test_account(new_account, app):
 
         # check password hash
         assert account.password != "password"
+
+        new_email = "newemail@test.py"
+        new_username = "newusername"
+        new_password = "newpassword"
+
+        account.update_email(new_email)
+        account.update_username(new_username)
+        account.update_password(new_password)
+
+        assert account.email == new_email
+        assert account.username == new_username
+        assert account.password != new_password
+        assert str(account) == f"User {account.username}"
+
+        account.delete()
+
+        account = Account.query.filter_by(email=new_email).first()
+
+        assert account is None
+
+
+def test_token(new_token, app):
+    with app.app_context():
+        new_token.save()
+        token = ResetToken.query.filter_by(username="testuser").first()
+
+        assert token is not None
+
+        token.delete()
+        token = ResetToken.query.filter_by(username="testuser").first()
+
+        assert token is None
+
+
+def test_game(new_game, app):
+    with app.app_context():
+        new_game.save()
+        game = Game.query.filter_by(id=1).first()
+
+        new_fen = "other_fen"
+        outcome = "BLACK WIN"
+        game.update_fen(new_fen)
+        game.set_end_time()
+        game.set_result(outcome)
+        game_dict = game.to_dict()
+
+        assert game is not None
+        assert game.fen == new_fen
+        assert game.end_time is not None
+        assert game.result == outcome
+        assert game.id in game_dict.values()
+        assert game.start_time.strftime("%H:%M %d %B %Y") in game_dict.values()
+        assert game.end_time.strftime("%H:%M %d %B %Y") in game_dict.values()
+        assert game.fen in game_dict.values()
+        assert game.result in game_dict.values()
+
+        game.delete()
+        game = Game.query.filter_by(id=1).first()
+
+        assert game is None
--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -5,20 +5,58 @@ from flask_jwt_extended import create_access_token
 from pytest_mock import mocker
 from datetime import datetime, timezone, timedelta
 from freezegun import freeze_time
+import pytest
+from flask_wtf.csrf import generate_csrf
 
-def test_registration_success(client):
-    response = client.post(
-        "/registration",
-        json={
-            "username": "testuser",
-            "email": "testuser@test.com",
-            "password": "testpassword",
-            "passwordRepeat": "testpassword",
-        },
-    )
 
-    assert response.status_code == 201
-    assert b"Successfully registered." in response.data
+@pytest.mark.parametrize(
+    "data, expected_status, message",
+    [
+        (
+            {
+                "username": "testuser",
+                "email": "testuser@test.com",
+                "password": "testpassword",
+                "passwordRepeat": "testpassword",
+            },
+            201,
+            b"Successfully registered.",
+        ),
+        (
+            {
+                "username": "testuser",
+                "email": "testuser@test.com",
+                "password": "testpassword",
+                "passwordRepeat": "differentpassword",
+            },
+            202,
+            b"Passwords do not match",
+        ),
+        (
+            {
+                "username": "testuser",
+                "email": "testuser@test.com",
+                "password": "tes",
+                "passwordRepeat": "tes",
+            },
+            202,
+            b"Password must be >=4 characters long",
+        ),
+        (
+            {
+                "username": "testuser",
+                "email": "testuser@test.com",
+                "password": "testpassword",
+            },
+            202,
+            b"Missing data",
+        ),
+    ],
+)
+def test_registartion(client, data, expected_status, message):
+    response = client.post("/registration", json=data)
+    assert response.status_code == expected_status
+    assert message in response.data
 
 
 def test_registration_existing_account(app, client, new_account):
@@ -43,50 +81,6 @@ def test_registration_existing_account(app, client, new_account):
         db.session.commit()
 
 
-def test_registration_different_passwords(client):
-    response = client.post(
-        "/registration",
-        json={
-            "username": "testuser",
-            "email": "testuser@test.com",
-            "password": "testpassword",
-            "passwordRepeat": "differentpassword",
-        },
-    )
-
-    assert response.status_code == 202
-    assert b"Passwords do not match" in response.data
-
-
-def test_registration_data_lenght(client):
-    response = client.post(
-        "/registration",
-        json={
-            "username": "testuser",
-            "email": "testuser@test.com",
-            "password": "tes",
-            "passwordRepeat": "tes",
-        },
-    )
-
-    assert response.status_code == 202
-    assert b"Password must be >=4 characters long" in response.data
-
-
-def test_registration_missing_data(client):
-    response = client.post(
-        "/registration",
-        json={
-            "username": "testuser",
-            "email": "testuser@test.com",
-            "password": "testpassword",
-        },
-    )
-
-    assert response.status_code == 202
-    assert b"Missing data" in response.data
-
-
 def test_registration_use_POST(client):
     response = client.get(
         "/registration",
@@ -101,65 +95,53 @@ def test_registration_use_POST(client):
     assert b"Use POST" in response.data
 
 
-def test_login_success(app, client):
+@pytest.mark.parametrize(
+    "data, expected_status, message",
+    [
+        (
+            {
+                "username": "testuser",
+                "password": "testpassword",
+            },
+            200,
+            b'"login":true',
+        ),
+        (
+            {
+                "username": "testuser",
+                "password": "wrongpassword",
+            },
+            200,
+            b"Incorrect Password.",
+        ),
+        (
+            {
+                "username": "testuser",
+            },
+            201,
+            b"Missing data.",
+        ),
+        (
+            {
+                "username": "no_user",
+                "password": "testpassword",
+            },
+            200,
+            b"Account does not exist.",
+        ),
+    ],
+)
+def test_login(app, client, data, expected_status, message):
     with app.app_context():
         account = Account(username="testuser", email="testuser@test.com")
         account.set_password("testpassword")
         db.session.add(account)
         db.session.commit()
 
-        response = client.post(
-            "/login",
-            json={
-                "username": "testuser",
-                "password": "testpassword",
-            },
-        )
+        response = client.post("/login", json=data)
 
-        assert b"access_token" in response.data
-        assert b"refresh_token" in response.data
-
-
-def test_login_incorrect_password(app, client, new_account):
-    with app.app_context():
-        db.session.add(new_account)
-        db.session.commit()
-
-        response = client.post(
-            "/login",
-            json={
-                "username": "testuser",
-                "password": "wrongpassword",
-            },
-        )
-
-        assert response.status_code == 200
-        assert b"Incorrect Password." in response.data
-
-
-def test_login_missing_data(app, client, new_account):
-    with app.app_context():
-        db.session.add(new_account)
-        db.session.commit()
-
-        response = client.post(
-            "/login",
-            json={
-                "username": "testuser",
-            },
-        )
-
-        assert response.status_code == 201
-        assert b"Missing data." in response.data
-
-
-def test_login_no_account(client):
-    response = client.post(
-        "/login", json={"username": "testuser", "password": "password"}
-    )
-
-    assert response.status_code == 200
-    assert b"Account does not exist." in response.data
+        assert message in response.data
+        assert response.status_code == expected_status
 
 
 def test_login_use_POST(client):
@@ -170,36 +152,67 @@ def test_login_use_POST(client):
     assert response.status_code == 201
     assert b"Use POST" in response.data
 
+
+def test_logout(app, client, new_account):
+    with app.app_context():
+        db.session.add(new_account)
+        db.session.commit()
+        token = create_access_token(identity=new_account.id)
+        client.set_cookie("localhost", "access_token_cookie", token)
+        response = client.post("/logout")
+        assert b'"logout":true' in response.data
+
+
 def test_show_account_no_account(app, client, new_account):
     with app.app_context():
         token = create_access_token(identity=new_account.id)
-        client.set_cookie('localhost', 'access_token_cookie', token)
-        response = client.get(
-            "/profile"
-        )
-        assert b'Account does not exist.' in response.data
+        client.set_cookie("localhost", "access_token_cookie", token)
+        response = client.get("/profile")
+        assert b"Account does not exist." in response.data
+
 
 def test_show_account_token_expired(app, client, new_account):
     with app.app_context():
         expires_delta = timedelta(seconds=1)
-        token = create_access_token(identity=new_account.id, expires_delta=expires_delta)
-        expired_timestamp = datetime.fromtimestamp(datetime.timestamp(datetime.now(timezone.utc)) + 2, timezone.utc)
+        token = create_access_token(
+            identity=new_account.id, expires_delta=expires_delta
+        )
+        expired_timestamp = datetime.fromtimestamp(
+            datetime.timestamp(datetime.now(timezone.utc)) + 2, timezone.utc
+        )
 
         with freeze_time(expired_timestamp):
-            client.set_cookie('localhost', 'access_token_cookie', token)
+            client.set_cookie("localhost", "access_token_cookie", token)
 
-            response = client.get('/profile')
+            response = client.get("/profile")
 
             assert response.status_code == 401
-            assert b'Token has expired' in response.data
+            assert b"Token has expired" in response.data
 
-def test_show_account(app, client, new_account):
+
+def test_show_account_success(app, client, new_account):
     with app.app_context():
-        expires_delta = timedelta(seconds=1)
-        token = create_access_token(identity=new_account.id, expires_delta=expires_delta)
-        expired_timestamp = datetime.fromtimestamp(datetime.timestamp(datetime.now(timezone.utc)) + 2, timezone.utc)
+        db.session.add(new_account)
+        db.session.commit()
+        token = create_access_token(identity=new_account.id)
+        client.set_cookie("localhost", "access_token_cookie", token)
 
-        with freeze_time(expired_timestamp):
-            client.set_cookie('localhost', 'access_token_cookie', token)
+        response = client.get("/profile")
 
-            response = client.get('/profile')
+        assert str(new_account.id) in str(response.data)
+        assert str(new_account.email) in str(response.data)
+        assert str(new_account.username) in str(response.data)
+
+
+def test_newgame_auth(client, app, new_account):
+    with app.app_context():
+        db.session.add(new_account)
+        db.session.commit()
+        token = create_access_token(identity=new_account.id)
+        client.set_cookie("localhost", "access_token_cookie", token)
+
+        response = client.get("/creategame")
+        id = response.get_json().get("id")
+
+        assert bytes('"id":"%s"' % id, "utf-8") in response.data
+
